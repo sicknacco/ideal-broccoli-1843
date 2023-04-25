f48156956aec7720409d0683c517846a528b603c
Nick Sacco <nicksacco10@gmail.com>
2023-04-25 08:52:10 -0700

    Complete test data setup

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,39 @@
+def test_data
+  @gard1 = Garden.create!(name: "Park Garden", organic: true)
+  @gard2 = Garden.create!(name: "Other Garden", organic: false)
+
+  @plot1 = @gard1.plot.create!(number: 1, size: "Little", direction: "East")
+  @plot2 = @gard1.plot.create!(number: 2, size: "Big", direction: "West")
+
+  @plot3 = @gard2.plot.create!(number: 3, size: "Gigantic", direction: "East")
+  @plot4 = @gard2.plot.create!(number: 4, size: "Tiny", direction: "East")
+
+  @tomato = Plant.create!(name: "Tomato", description: "Grows like a weed", days_to_harvest: 90)
+  @radish = Plant.create!(name: "Rasish", description: "Plain Radish", days_to_harvest: 120)
+  @beans = Plant.create!(name: "Beans", description: "Looks like a bean", days_to_harvest: 45)
+  @peas = Plant.create!(name: "Peas", description: "Green Peas", days_to_harvest: 34)
+  @squash = Plant.create!(name: "Squash", description: "Big yellow ones", days_to_harvest: 100)
+  @pineapple = Plant.create!(name: "Pineapple", description: "Grows out of the ground!", days_to_harvest: 60)
+
+  PlotPlant.create!(plot: @plot1, plant:@tomato)
+  PlotPlant.create!(plot: @plot1, plant:@peas)
+  PlotPlant.create!(plot: @plot1, plant:@beans)
+
+  PlotPlant.create!(plot: @plot2, plant:@beans)
+  PlotPlant.create!(plot: @plot2, plant:@pineapple)
+  PlotPlant.create!(plot: @plot2, plant:@squash)
+
+  PlotPlant.create!(plot: @plot3, plant:@radish)
+  PlotPlant.create!(plot: @plot3, plant:@peas)
+  PlotPlant.create!(plot: @plot3, plant:@squash)
+
+  PlotPlant.create!(plot: @plot4, plant:@tomato)
+  PlotPlant.create!(plot: @plot4, plant:@pineapple)
+  PlotPlant.create!(plot: @plot4, plant:@radish)
+end
+
+
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
