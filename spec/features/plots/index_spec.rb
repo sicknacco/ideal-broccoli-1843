c8869c314dd5c2e90c7f0d476a642628eb9f9ce6
Nick Sacco <nicksacco10@gmail.com>
2023-04-25 09:50:30 -0700

    us2 setup. tests, view, next is creating plot_plant controller

--- spec/features/plots/index_spec.rb
+++ spec/features/plots/index_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe "/plots Plots Index Page", type: :feature do
   describe "When I visit the plots index page ('/plots')" do
     it "I see a list of all plot numbers and all of that plots plants" do
       visit '/plots'
-
+      
       within "#plot_info_#{@plot1.id}" do
         expect(page).to have_content(@plot1.number)
         expect(page).to have_content(@tomato.name)
@@ -22,6 +22,23 @@ RSpec.describe "/plots Plots Index Page", type: :feature do
         expect(page).to have_content(@beans.name)
         expect(page).to have_content(@pineapple.name)
         expect(page).to have_content(@squash.name)
+        expect(page).to_not have_content(@tomato.name)
+      end
+    end
+  end
+  
+  describe "Next to each plant's name I see a link to remove plant from this plot" do
+    it 'when I click the delete plant button I am returned to the plots index page' do
+      visit '/plots'
+
+      within "#plot_info_#{@plot1.id}" do
+        expect(page).to have_link("Delete #{@tomato.name}")
+        expect(page).to have_link("Delete #{@peas.name}")
+        expect(page).to have_link("Delete #{@beans.name}")
+
+        click_link("Delete Tomato")
+        expect(current_path).to eq('/plots')
+
         expect(page).to_not have_content(@tomato.name)
       end
     end
