03f9f66879a943874c518f51e3c4d208018a1ece
Nick Sacco <nicksacco10@gmail.com>
2023-04-25 10:34:34 -0700

    NOT COMPLETE US2 WIP

--- app/controllers/plot_plants_controller.rb
+++ app/controllers/plot_plants_controller.rb
@@ -1,7 +1,14 @@
 class PlotPlantsController < ApplicationController
   def destroy
-    plot_plant = PlotPlant.find_by(plot_id: params[:id], plant_id:[:id])
-    plot_plant.destroy
+    # require 'pry'; binding.pry
+    plot_plant = PlotPlant.find_by(plot_id: params[:id], plant_id: params[:id])
+    plot_plant.delete
     redirect_to '/plots'
+    ## tried to change the plant_id: params[:everything I could think of]
+    ## plant: params[:id]
+    ## plant_id: params[plant]
+    ## plant_id: params[@plant.id]
+    ## plant_id: params[plant.id]
+    ## plant_id: params[plant_id]
   end
 end
