8ebdc8336a18a27bbbc0225241ef431c6cf1867c
Nick Sacco <nicksacco10@gmail.com>
2023-04-25 09:27:29 -0700

    test: setup within block tests for us1

--- spec/features/plots/index_spec.rb
+++ spec/features/plots/index_spec.rb
@@ -11,7 +11,18 @@ RSpec.describe "/plots Plots Index Page", type: :feature do
 
       within "#plot_info_#{@plot1.id}" do
         expect(page).to have_content(@plot1.number)
-        expect(page).to have_content(@plot1.plants)
+        expect(page).to have_content(@tomato.name)
+        expect(page).to have_content(@peas.name)
+        expect(page).to have_content(@beans.name)
+        expect(page).to_not have_content(@pineapple.name)
+      end
+      
+      within "#plot_info_#{@plot2.id}" do
+        expect(page).to have_content(@plot2.number)
+        expect(page).to have_content(@beans.name)
+        expect(page).to have_content(@pineapple.name)
+        expect(page).to have_content(@squash.name)
+        expect(page).to_not have_content(@tomato.name)
       end
     end
   end
