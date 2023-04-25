49919384f0998a807c9ae765d157aae373c2b08e
Nick Sacco <nicksacco10@gmail.com>
2023-04-25 10:51:19 -0700

    wip

--- app/controllers/plot_plants_controller.rb
+++ app/controllers/plot_plants_controller.rb
@@ -1,12 +1,13 @@
 class PlotPlantsController < ApplicationController
   def destroy
-    # require 'pry'; binding.pry
+    require 'pry'; binding.pry
     plot_plant = PlotPlant.find_by(plot_id: params[:id], plant_id: params[:id])
     plot_plant.delete
     redirect_to '/plots'
     ## tried to change the plant_id: params[:everything I could think of]
     ## plant: params[:id]
     ## plant_id: params[plant]
+    ## plant_id: params[:plant]
     ## plant_id: params[@plant.id]
     ## plant_id: params[plant.id]
     ## plant_id: params[plant_id]
---
+++ spec/features/plots/gardens/show_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe "/gardens/:id", type: :feature do
+  before(:each) do
+    test_data
+  end
+end
